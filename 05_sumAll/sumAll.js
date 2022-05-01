3a2ddcc82a82f948f0ac933b42410f559e164f40
Asad Mahmood <asadmhmd1234@gmail.com>
2022-05-01 18:00:20 +0530

    Complete sum all exercise and test it against js spec file

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
 
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    return "ERROR";
+  }
+  let start = num1 < num2 ? num1 : num2;
+  let end = num2 < num1 ? num1 : num2;
+  let result = 0;
+  for (let i = start; i <= end; i++) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
