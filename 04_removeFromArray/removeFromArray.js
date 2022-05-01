be25c44ba921205f8b3543ce9fddc7f166f0fe44
Asad Mahmood <asadmhmd1234@gmail.com>
2022-05-01 17:49:53 +0530

    Complete removal from array exercise and test it

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...element) {
+  let result = [];
 
+  for (let i = 0; i < arr.length; i++) {
+    let flag = false;
+
+    for (let ele of element) {
+      if (ele === arr[i]) {
+        flag = true;
+      }
+    }
+
+    if (!flag) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
