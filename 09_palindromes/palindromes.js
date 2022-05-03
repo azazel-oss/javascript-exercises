2d6e75dc400e25148986cfb42d9c33a18916748e
Asad Mahmood <asadmhmd1234@gmail.com>
2022-05-03 12:42:44 +0530

    Complete palindrome exercise and test it

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let stringToCheck = "";
+  for (let char of string.toLowerCase()) {
+    if (!/[^a-z]/.test(char)) stringToCheck += char;
+  }
 
+  let i = 0;
+  let j = stringToCheck.length - 1;
+  for (i, j; i < j; i++, j--) {
+    if (stringToCheck[i] !== stringToCheck[j]) return false;
+  }
+  return true;
 };
 
 // Do not edit below this line
