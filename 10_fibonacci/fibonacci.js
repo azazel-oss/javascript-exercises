9149fd3fd8003511c173388bbaca5cc335a6b784
Asad Mahmood <asadmhmd1234@gmail.com>
2022-05-03 13:02:14 +0530

    Complete the fibonacci exercise and test it

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (a) {
+  a = Number.parseInt(a);
+  if (a < 1) {
+    return "OOPS";
+  }
+  let result = [1, 1];
+  if (a === 1 || a === 2) {
+    return 1;
+  }
+  for (let i = 2; i < a; i++) {
+    result.push(result[i - 1] + result[i - 2]);
+  }
+  return result.slice(-1)[0];
 };
 
 // Do not edit below this line
