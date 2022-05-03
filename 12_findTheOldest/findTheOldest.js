dae6eb064860f0e5d055bd89be20f3d394a4fef0
Asad Mahmood <asadmhmd1234@gmail.com>
2022-05-03 14:23:07 +0530

    Complete the findTheOldest exercise and test it

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((a, b) => {
+    let currentDeathYear = b.yearOfDeath || new Date().getFullYear();
+    let previousDeathYear = a.yearOfDeath || new Date().getFullYear();
 
+    if (currentDeathYear - b.yearOfBirth > previousDeathYear - a.yearOfBirth) {
+      return b;
+    }
+    return a;
+  }, people[0]);
 };
 
 // Do not edit below this line
